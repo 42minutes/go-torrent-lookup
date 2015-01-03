ac91df04b8d3c8482c60fbe36140edc5357de308
Paris Goudas <paris@gordius.gr>
2015-01-03 03:58:04 +0200

    Add parse pages and find magnet links

--- app.go
+++ app.go
@@ -2,22 +2,60 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
-	"net/http"
+	"github.com/PuerkitoBio/goquery"
 	"strings"
 )
 
+type Result struct {
+	Name   string
+	Manget map[string]string
+}
+
 func main() {
 	term := "Big Bang Theory"
-
-	resp, err := http.Get("https://torrentz.eu/search?q=" + strings.Replace(term, " ", "+", -1))
+	fmt.Println("https://torrentz.eu/search?q=" + strings.Replace(term, " ", "+", -1))
+	doc, err := goquery.NewDocument("https://torrentz.eu/search?q=" + strings.Replace(term, " ", "+", -1))
 	if err == nil {
-		body, err := ioutil.ReadAll(resp.Body)
-		if err == nil {
-			fmt.Println(body)
-		}
-
-	} else {
-		fmt.Println(err)
+		doc.Find(".results dl dt").Each(func(i int, s *goquery.Selection) {
+			link, _ := s.Find("a").Attr("href")
+			name := s.Find("a").Text()
+			fmt.Println(name)
+			results := ListResultPages("https://torrentz.eu" + link)
+			for site, link := range results {
+				fmt.Println(site)
+				FindMagnet(link)
+			}
+		})
 	}
+}
+
+func ListResultPages(url string) map[string]string {
+	results := make(map[string]string)
+	doc, err := goquery.NewDocument(url)
+	if err == nil {
+		doc.Find(".download dl").Each(func(i int, s *goquery.Selection) {
+			dd := s.Find("dd").Text()
+			if dd != "Sponsored Link" {
+				link, _ := s.Find("dt a").Attr("href")
+				name := s.Find("dt a span.u").Text()
+				results[name] = link
+				// fmt.Println(name, link)
+			}
+		})
+	}
+	return results
+}
+
+func FindMagnet(url string) map[string]string {
+	doc, err := goquery.NewDocument(url)
+	if err == nil {
+		doc.Find("a").Each(func(i int, s *goquery.Selection) {
+			link, _ := s.Attr("href")
+			// fmt.Println(link)
+			if strings.Contains(string(link), "magnet:") {
+				fmt.Println(link)
+			}
+		})
+	}
+	return nil
 }
