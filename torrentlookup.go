ec379dbd7fc44beed5608ffeeebb272ab3fca331
Paris Goudas <superdecimal@users.noreply.github.com>
2017-01-22 17:41:43 +0000

    Add method to return all results (#3)
    * Add method to return all results
    
    * Replace Search with SearchAll
    
    * Change Search to return pointer
    
    * Change Search result to array of pointers
    
    * Minor change το torrent pointer

--- torrentlookup.go
+++ torrentlookup.go
@@ -19,14 +19,22 @@ type Provider struct {
 	SeedsSubQuery  string
 }
 
+// Torrent -
+type Torrent struct {
+	Name     string
+	Infohash string
+}
+
 // Search allows finding magnet links in the provider
-func (provider *Provider) Search(query string) (name, infohash string, err error) {
+func (provider *Provider) Search(query string) ([]*Torrent, error) {
+	results := []*Torrent{}
 	searchURL := fmt.Sprintf(provider.SearchURL, url.QueryEscape(query))
 	doc, err := goquery.NewDocument(searchURL)
 	if err != nil {
-		return
+		return nil, err
 	}
-	doc.Find(provider.RowQuery).EachWithBreak(func(i int, s *goquery.Selection) bool {
+	doc.Find(provider.RowQuery).Each(func(i int, s *goquery.Selection) {
+		var infohash, name string
 		seeds, _ := strconv.Atoi(s.Find(provider.SeedsSubQuery).First().Text())
 		name = s.Find(provider.NameSubQuery).First().Text()
 		magnet, _ := s.Find(provider.MagnetSubQuery).First().Attr("href")
@@ -34,11 +42,15 @@ func (provider *Provider) Search(query string) (name, infohash string, err error
 			infohash = getInfohashFromMagnet(magnet)
 		}
 		if seeds > 0 && infohash != "" {
-			return false
+			tor := &Torrent{
+				Name:     name,
+				Infohash: infohash,
+			}
+			results = append(results, tor)
 		}
-		return true
+
 	})
-	return name, infohash, nil
+	return results, nil
 }
 
 func getInfohashFromMagnet(magnet string) (infohash string) {
