4a97a27425674b828c37ff7336ed19115e97b730
Paris Goudas <paris@darkstar.gr>
2017-01-22 11:25:16 +0000

    Add tor2eu provider

--- providers.go
+++ providers.go
@@ -9,3 +9,13 @@ var ProviderTPB = &Provider{
 	MagnetSubQuery: "td:nth-child(2) > a:nth-child(2)",
 	SeedsSubQuery:  "td:nth-child(3)",
 }
+
+// ProviderTorzeu -
+var ProviderTorzeu = &Provider{
+	Name:           "Torrentz2.eu",
+	SearchURL:      "https://torrentz2.eu/verified?f=%s",
+	RowQuery:       "dl",
+	NameSubQuery:   "dt>a",
+	MagnetSubQuery: "dt>a",
+	SeedsSubQuery:  "dd>span:nth-child(4)",
+}
